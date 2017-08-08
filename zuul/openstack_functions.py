8c14f084539671ce293a8f3bf23547d6f8173622
Rob Cresswell <rcresswe@cisco.com>
2017-08-08 08:45:44 -0400

    Update zuul openstack functions

--- zuul/openstack_functions.py
+++ zuul/openstack_functions.py
@@ -12,27 +12,53 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import re
+
 
 def set_log_url(item, job, params):
     if hasattr(item.change, 'refspec'):
-        path = "%s/%s/%s/%s" % (
+        path = "%s/%s/%s/%s/" % (
             params['ZUUL_CHANGE'][-2:], params['ZUUL_CHANGE'],
             params['ZUUL_PATCHSET'], params['ZUUL_PIPELINE'])
     elif hasattr(item.change, 'ref'):
-        path = "%s/%s/%s" % (
+        path = "%s/%s/%s/" % (
             params['ZUUL_NEWREV'][:2], params['ZUUL_NEWREV'],
             params['ZUUL_PIPELINE'])
     else:
-        path = params['ZUUL_PIPELINE']
+        path = params['ZUUL_PIPELINE'] + '/'
     params['BASE_LOG_PATH'] = path
-    params['LOG_PATH'] = path + '/%s/%s' % (job.name,
+    params['LOG_PATH'] = path + '%s/%s/' % (job.name,
                                             params['ZUUL_UUID'][:7])
 
 
-def single_use_node(item, job, params):
-    set_log_url(item, job, params)
-    params['OFFLINE_NODE_WHEN_COMPLETE'] = '1'
-
-
 def reusable_node(item, job, params):
+    if 'OFFLINE_NODE_WHEN_COMPLETE' in params:
+        del params['OFFLINE_NODE_WHEN_COMPLETE']
+
+
+def set_node_options(item, job, params):
+    # Force tox to pass through ZUUL_ variables
+    zuul_params = [x for x in params.keys() if x.startswith('ZUUL_')]
+    params['TOX_TESTENV_PASSENV'] = ' '.join(zuul_params)
+    # Set up log url parameter for all jobs
     set_log_url(item, job, params)
+    # Default to single use node. Potentially overridden below.
+    # Select node to run job on.
+    params['OFFLINE_NODE_WHEN_COMPLETE'] = '1'
+    # Pass tags through for subunit2sql
+    params['JOB_TAGS'] = ' '.join(sorted(job.tags))
+    proposal_re = r'^.*(propose|upstream)-(.*?)-(constraints-.*|updates?|update-(liberty|mitaka|newton|ocata|pike)|plugins-list|openstack-constraints|update-constraints|osa-test-scripts)$'  # noqa
+    release_re = r'^.*-(forge|jenkinsci|mavencentral|pypi-(both|wheel)|npm)-upload$'
+    hook_re = r'^hook-(.*?)-(rtfd)$'
+    wheel_re = r'^wheel-(build|release)-.*$'
+    reprepro_re = r'^reprepro-(import|release|sign)-.*$'
+    signing_re = r'^(.*-tarball-signing|tag-releases)$'
+    # jobs run on the persistent proposal, release, signing, and wheel
+    # build workers
+    if (re.match(proposal_re, job.name) or
+        re.match(release_re, job.name) or
+        re.match(hook_re, job.name) or
+        re.match(reprepro_re, job.name) or
+        re.match(signing_re, job.name) or
+        re.match(wheel_re, job.name)):
+        reusable_node(item, job, params)
