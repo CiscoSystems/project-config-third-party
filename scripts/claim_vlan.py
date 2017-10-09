5c5de666e86d6f2627df79762b0e3b0f188861d4
Sam Betts <sam@code-smash.net>
2017-10-09 16:53:43 +0100

    Fix timestamp comparison on ciresources

--- scripts/claim_vlan.py
+++ scripts/claim_vlan.py
@@ -12,7 +12,7 @@ f = '%Y-%m-%d %H:%M:%S'
 three_hours_ago_dt = datetime.utcnow() - timedelta(hours=3)
 three_hours_ago = three_hours_ago_dt.strftime(f)
 
-cur.execute("SELECT * FROM vlans WHERE locked!=true OR timestamp > '%s' LIMIT 1 FOR UPDATE" % three_hours_ago)
+cur.execute("SELECT * FROM vlans WHERE locked!=true OR timestamp<'%s' LIMIT 1 FOR UPDATE" % three_hours_ago)
 
 row = cur.fetchone()
 
